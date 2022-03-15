382d890ab5fdf8369c74fd02e4d283ed062200e8
Kamila Kowalczyk <kamila.kowalczyk@icloud.com>
2022-03-14 22:15:42 -0400

    kind of working right now

--- index.js
+++ index.js
@@ -1,23 +1,42 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require("./data");
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
 
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI)
-  .then(x => {
+  .then((x) => {
     console.log(`Connected to the database: "${x.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+    return Recipe.deleteMany();
   })
   .then(() => {
-   return Recipe.create(data[0])
+    return Recipe.create(data[0]);
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
+  .then(() => {
+    return Recipe.deleteOne(data[0]);
+  })
+  .then(() => {
+    return Recipe.insertMany(data);
+  })
+  .then(() => {
+    return Recipe.find({}, 'title', (err, docs) => {
+      if(err){
+        console.log(err)
+
+        return
+      }
+      console.log(docs)
+
+    })
+    //print the title of each recipe to console
+    //console.log(title of recipe)
+  })
+  .catch((error) => {
+    console.error("Error connecting to the database", error);
   });
